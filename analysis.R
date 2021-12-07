6763c0c4ddfc4026eb08910281b931b30178eca4
wphan-1925132 <wphan@uw.edu>
2021-12-07 00:00:38 -0800

    REVISED

--- analysis.R
+++ analysis.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(maps)
 library(mapproj)
-trends <- read.csv("~/Junior UW/incarceration-trends/incarceration_trends.csv")
+
+trends <- read.csv("incarceration_trends.csv")
 
 # 1. Which County has the largest black jail population?
 highest_black_county <- trends %>%
@@ -42,7 +43,8 @@ white_highest_year
 # 3. Which State has the largest black jail population?
 highest_black_state <- trends %>%
   group_by(state) %>%
-  summarize(black_jail_pop = max(black_jail_pop, na.rm = TRUE, infinite.rm = TRUE)) %>%
+  summarize(black_jail_pop = max(black_jail_pop, na.rm = TRUE, 
+                                 infinite.rm = TRUE)) %>%
   filter(black_jail_pop == max(black_jail_pop)) %>%
   pull(state)
 
@@ -51,7 +53,8 @@ highest_black_state
 # 3. Which State has the largest white jail population?
 highest_white_state <- trends %>%
   group_by(state) %>%
-  summarize(white_jail_pop = max(white_jail_pop, na.rm = TRUE, infinite.rm = TRUE)) %>%
+  summarize(white_jail_pop = max(white_jail_pop, na.rm = TRUE, 
+                                 infinite.rm = TRUE)) %>%
   filter(white_jail_pop == max(white_jail_pop)) %>%
   pull(state)
 
@@ -81,7 +84,8 @@ highest_white_ny
 mean_black_pop <- trends %>%
   group_by(state) %>%
   filter(year == max(year)) %>%
-  summarize(black_pop = max(black_jail_pop, na.rm = TRUE, infinite.rm = TRUE)) %>%
+  summarize(black_pop = max(black_jail_pop, na.rm = TRUE, 
+                            infinite.rm = TRUE)) %>%
   summarize(black_pop = mean(black_pop)) %>%
   pull(black_pop)
 
@@ -91,7 +95,8 @@ mean_black_pop
 mean_white_pop <- trends %>%
   group_by(state) %>%
   filter(year == max(year)) %>%
-  summarize(white_pop = max(white_jail_pop, na.rm = TRUE, infinite.rm = TRUE)) %>%
+  summarize(white_pop = max(white_jail_pop, na.rm = TRUE, 
+                            infinite.rm = TRUE)) %>%
   summarize(white_pop = mean(white_pop)) %>%
   pull(white_pop)
 
@@ -104,43 +109,66 @@ ny_black_jail <- trends %>%
   summarize(black_jail_pop = max(black_jail_pop, na.rm = TRUE))
 
 # Black Imprisonment Trend plot
-black_ny_plot <- ggplot(data = ny_black_jail, mapping = aes(x = year, 
-                                                      y = black_jail_pop)) +
-  geom_bar(mapping = aes(fill = year), stat = 'identity') +
-  labs(x = "Year", y = "Cumulative Count", 
-       title = "Black Imprisonment Trend in NY")
-                            
+black_ny_plot <- ggplot(data = ny_black_jail, mapping = aes(
+  x = year,
+  y = black_jail_pop
+)) +
+  geom_bar(mapping = aes(fill = year), stat = "identity") +
+  labs(
+    x = "Year", y = "Cumulative Count",
+    title = "Black Imprisonment Trend in NY"
+  )
+
 black_ny_plot
 
-#I included this chart because the incarceration of POC, specifically black
-#people continues to be an extremely relevant topic in the United States. I 
-#wanted to see how black incarceration rates have changed over the years. 
-#Looking at the graph, it appears that there was a steady growth of incarceration
-#rates starting in 1985. It peaked in 1993 and then had a gradual downfall which
-#continues on into recent years.
+# I included this chart because the incarceration of POC, specifically black
+# people continues to be an extremely relevant topic in the United States. I
+# wanted to see how black incarceration rates have changed over the years.
+# Looking at the graph, it appears that there was a steady growth of incarceration
+# rates starting in 1985. It peaked in 1993 and then had a gradual downfall which
+# continues on into recent years.
 
 # Filter white_jail_pop
 ny_white_jail <- trends %>%
   group_by(state = "NY") %>%
   group_by(year) %>%
-  summarize(white_jail_pop = max(white_jail_pop, na.rm = TRUE)) 
+  summarize(white_jail_pop = max(white_jail_pop, na.rm = TRUE))
 
 ny_white_jail
 
 # White Imprisonment Trend plot
-white_ny_plot <- ggplot(data = ny_white_jail, mapping = aes(x = year, 
-                                                           y = white_jail_pop)) +
-  geom_bar(mapping = aes(fill = year), stat = 'identity') +
-  labs(x = "Year", y = "Cumulative Count", 
-       title = "White Imprisonment Trend in NY")
+white_ny_plot <- ggplot(data = ny_white_jail, mapping = aes(
+  x = year,
+  y = white_jail_pop
+)) +
+  geom_bar(mapping = aes(fill = year), stat = "identity") +
+  labs(
+    x = "Year", y = "Cumulative Count",
+    title = "White Imprisonment Trend in NY"
+  )
 
 white_ny_plot
 
-#I created this chart because comparing the black incarceration trends to the
-#white incarceration trends is a great way to show the disproportion of the 
-#incarceration of POC, specifically black inmates. Comparing the two datasets,
-#on average, more black people are imprisoned annually than white people. A 
-#pattern that emerged from both datasets was the spike in imprisonments in 1993.
+nytest <- trends %>%
+  group_by(state = "NY") %>%
+  group_by(year) %>%
+  summarize(white_jail_pop = max(white_jail_pop, na.rm = TRUE),
+            black_jail_pop = max(black_jail_pop, na.rm = TRUE))
+
+test <- ggplot(data = nytest, mapping = aes(
+  x = black_jail_pop, y = white_jail_pop
+)) +
+  geom_point(color = "blue") +
+  labs(x = "Black Jail Population",
+       y = "White Jail Population",
+       title = "Black vs. White Imprisonment in NY")
+
+test
+# I created this chart because comparing the black incarceration trends to the
+# white incarceration trends is a great way to show the disproportion of the
+# incarceration of POC, specifically black inmates. Comparing the two datasets,
+# on average, more black people are imprisoned annually than white people. A
+# pattern that emerged from both datasets was the spike in imprisonments in 1993.
 
 # Get most recent counties data from dataset
 counties_mod <- trends %>%
@@ -154,7 +182,7 @@ county_shapes <- map_data("county") %>%
 
 # Merge map data
 map_data <- county_shapes %>%
-  left_join(counties_mod, by= "fips")
+  left_join(counties_mod, by = "fips")
 
 # Incoporate blank theme
 blank_theme <- theme_bw() +
@@ -172,12 +200,14 @@ blank_theme <- theme_bw() +
 # Black Imprisonment Population Map
 b_pop_map <- ggplot(map_data) +
   geom_polygon(
-    mapping = aes(x= long, y= lat, group=group, fill = black_jail_pop),
+    mapping = aes(x = long, y = lat, group = group, fill = black_jail_pop),
     color = "gray", size = 0.3
   ) +
   coord_map() +
-  scale_fill_continuous(limits = c(0, max(map_data$black_jail_pop)), na.value = "white", low = "pink", high = "purple") +
+  scale_fill_continuous(limits = c(0, max(map_data$black_jail_pop)), 
+                        na.value = "white", low = "pink", high = "purple") +
   blank_theme +
+  labs(fill = "Black Jail Population") +
   ggtitle("2018 Black Imprisonment Population in the United States")
 
 b_pop_map
@@ -185,12 +215,14 @@ b_pop_map
 # White Imprisonment Population Map
 w_pop_map <- ggplot(map_data) +
   geom_polygon(
-    mapping = aes(x= long, y= lat, group=group, fill = white_jail_pop),
+    mapping = aes(x = long, y = lat, group = group, fill = white_jail_pop),
     color = "gray", size = 0.3
   ) +
   coord_map() +
-  scale_fill_continuous(limits = c(0, max(map_data$white_jail_pop)), na.value = "white", low = "pink", high = "purple") +
+  scale_fill_continuous(limits = c(0, max(map_data$white_jail_pop)), 
+                        na.value = "white", low = "pink", high = "purple") +
   blank_theme +
+  labs(fill = "White Jail Population") +
   ggtitle("2018 White Imprisonment Population in the United States")
 
 w_pop_map
